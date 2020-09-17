553642e312ad14aadf77f4da3b81004e93c70610
saddamhr <saddambubt65@gmail.com>
2020-09-17 16:44:10 +0600

    clean up

--- client/src/components/AddPerson/AddPerson.js
+++ client/src/components/AddPerson/AddPerson.js
@@ -58,10 +58,9 @@ class App extends React.Component {
 
             <div style={{marginTop: '20px'}} className="container admin-login">
                 <div style={{ margin: '20px' }}>
-                    {/* <a href="/" class="btn btn-lg btn-primary">Go Back</a> */}
+                    
                     <h2 className="text-center mb-3 display-4">Add New User</h2>
                     <hr className='admin-title-line' />
-                    {/* <h1 className="text-center">Admin Login</h1> */}
                     <form className='adminblog-form' onSubmit={this.submit}>
                         <div class="form-group">
                             <label for="exampleInputEmail1">Name</label>
@@ -109,68 +108,10 @@ class App extends React.Component {
                                 placeholder='Enter Email'
                             />
                         </div>
-                        {/* <input style={{ backgroundColor: '#1BCE7C', borderColor: '#1BCE7C', color: '#fff', fontWeight: 'bold' }} type='submit' value='POST' /> */}
-                        <button style={{ backgroundColor: '#1BCE7C', borderColor: '#1BCE7C', color: '#fff', padding: '5px' }} type="submit" class="btn btn-primary btn-lg btn-block mt-4">SignIn</button>
-                        {/* <button type="submit" class="btn btn-primary">Login</button> */}
+                        <button style={{ backgroundColor: '#1BCE7C', borderColor: '#1BCE7C', color: '#fff', padding: '5px' }} type="submit" class="btn btn-primary btn-lg btn-block mt-4">Add Person</button>
                     </form>
                 </div>
             </div>
-
-
-
-            // <div className='container'>
-            //     <div className="card-margin">
-            //         <h2 className="text-center mb-3 display-4">Add New Person</h2>
-            //         <hr className='admin-title-line' />
-            //         <div className='row mb-3'>
-            //             <div className='col-md-12 sm-margin'>
-            //                 <form className='adminblog-form' onSubmit={this.submit}>
-            //                     <div className='form-input'>
-            //                         <input
-            //                             type='text'
-            //                             name='name'
-            //                             value={this.state.name}
-            //                             onChange={this.handleChange}
-            //                             placeholder='Enter name'
-            //                         />
-            //                     </div>
-            //                     <div className='form-input'>
-            //                         <input
-            //                             type='text'
-            //                             name='district'
-            //                             value={this.state.district}
-            //                             onChange={this.handleChange}
-            //                             placeholder='Enter district'
-            //                         />
-            //                     </div>
-
-            //                     <div className='form-input'>
-            //                         <input
-            //                             type='text'
-            //                             name='phone'
-            //                             value={this.state.phone}
-            //                             onChange={this.handleChange}
-            //                             placeholder='Enter phone'
-            //                         />
-            //                     </div>
-            //                     <div className='form-input'>
-            //                         <input
-            //                             type='text'
-            //                             name='email'
-            //                             value={this.state.email}
-            //                             onChange={this.handleChange}
-            //                             placeholder='Enter Email'
-            //                         />
-            //                     </div>
-            //                     <input style={{ backgroundColor: '#1BCE7C', borderColor: '#1BCE7C', color: '#fff', fontWeight: 'bold' }} type='submit' value='ADD' />
-
-            //                 </form>
-            //             </div>
-
-
-            //         </div>
-            //     </div>
-            // </div>
         );
     }
 }
